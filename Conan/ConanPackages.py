8af33a97c071b176fad329034e59408f954130d4
sylsit <sitkowskis@interia.pl>
2020-06-12 20:01:06 +0200

    support for external packages added

--- Conan/ConanPackages.py
+++ Conan/ConanPackages.py
@@ -1,19 +1,23 @@
+from conans import tools
 import os, re
 
 class ConanPackages:
     def __createFolderDownload (self, v_downloadsPath):
         print ('createFolderDownload')
-        if not os.path.isdir(v_downloadsPath):
-            os.mkdir(v_downloadsPath)
-        os.chdir(v_downloadsPath)
+
+        if not os.path.isdir (v_downloadsPath):
+            os.mkdir (v_downloadsPath)
+        os.chdir (v_downloadsPath)
 
     def __cloneRepo (self, v_name, v_downloadsPath, v_repoUrl):
         print ('cloneRepo')
         url = v_repoUrl + '/' + v_name + '.git'
         print ('url', url)
-        if not os.path.isdir (v_downloadsPath):
-            self.run('git clone ' + url)
-        os.chdir(v_downloadsPath + '/' + v_name + '/Conan')
+
+        packageDownloadsPath = v_downloadsPath + '/' + v_name
+        if not os.path.isdir (packageDownloadsPath):
+            self.run ('git clone ' + url)
+        os.chdir (packageDownloadsPath + '/Conan')
 
     def __createPackage (self, v_user, v_channel):
         print ('createPackage')
@@ -21,39 +25,41 @@ class ConanPackages:
         
     def __parse (self, v_package):
         print ('parse')
+
         packageComponent = (re.split('[/@]', v_package, 3))
         return {'name' : packageComponent [0], 'version' : packageComponent [1], 'user' : packageComponent [2], 'channel' : packageComponent [3]}
 
     def GetPaths (self, v_packagesPath, v_packages):
         print ('GetPaths')
-        includesPaths = []
-        libsPaths     = []
-        libsNames     = []
 
+        paths    = {}
+        libNames = []
         for package in v_packages:
             packageComponent = ConanPackages.__parse (self, package)
             path             = v_packagesPath + '/' + packageComponent ['name'] + '/' + packageComponent ['version'] + '/' + packageComponent ['user'] + '/' + packageComponent ['channel'] + '/package'
-            hashFolder       = os.listdir(path)
+            hashFolder       = os.listdir (path)
             includePath      = path + '/' + hashFolder [0] + '/include'
             libPath          = path + '/' + hashFolder [0] + '/lib'
-            libName          = packageComponent ['name'] + 'Lib'
+            libName          = packageComponent ['name'] + 'Lib.lib'
+             
+            if not os.path.isdir (includePath):
+                raise Exception ('%s. Is not package include path', packageIncludePath)
 
-            if not os.path.isdir(includePath):
-                raise Exception ('%s. Is not package include path', includePath)
+            if not os.path.isdir (libPath):
+                raise Exception ('%s. Is not package include lib path', packagePath)
 
-            if not os.path.isdir(libPath):
-                raise Exception ('%s. Is not package include lib path', libPath)
+            paths [packageComponent ['name'] + 'IncludePath'] = includePath
+            paths [packageComponent ['name'] + 'LibPath']     = libPath
+            paths [packageComponent ['name'] + 'LibName']     = libName
+            libNames.append (packageComponent ['name'])
 
-            includesPaths.append(includePath)
-            libsPaths    .append(libPath)
-            libsNames    .append(libName)
-
-        return {'IncludesPaths' : includesPaths, 'LibsPaths' : libsPaths, 'LibsNames' : libsNames}
+        tools.replace_in_file (os.getcwd ().replace ('\Conan','') + "\\CMakeLists.txt", "PackagesTempNames", str (libNames).strip ('[]').replace (',','').replace ('\'', ''), False)    
+        return paths
 
     def Install (self, v_downloadsPath, v_repoUrl, v_packages):
         print ('Install')
         for package in v_packages:
             packageComponent = ConanPackages.__parse (self, package)
-            ConanPackages.__createFolderDownload (self, v_downloadsPath)
-            ConanPackages.__cloneRepo            (self, packageComponent ['name'], v_downloadsPath, v_repoUrl)
-            ConanPackages.__createPackage        (self, packageComponent ['user'], packageComponent ['channel'])
+            ConanPackages.__createFolderDownload     (self, v_downloadsPath)
+            ConanPackages.__cloneRepo                (self, packageComponent ['name'], v_downloadsPath, v_repoUrl)
+            ConanPackages.__createPackage            (self, packageComponent ['user'], packageComponent ['channel'])
--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -20,46 +20,46 @@ class Conan(ConanFile):
     requires        = ["gtest/1.8.1@bincrafters/stable"]
     DownloadsPath   = "C:/Users/sitko/.conan/download"
     PackagesPath    = "C:/Users/sitko/.conan/data"
-    Packages        = ["Logger/1.0@ssitkowx/stable"]
+    Packages        = ["Logger/1.0@ssitkowx/stable", "Utils/1.0@ssitkowx/stable"]
 
     def source (self):   
         ConanPackages.Install (self, self.DownloadsPath, self.repoUrl, self.Packages)
 
     def build (self):
-        projectPath  = os.getcwd().replace('\Conan','')
+        projectPath  = os.getcwd ().replace ('\Conan','')
         projectBuild = projectPath + '\\Build'
         
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+        if not os.path.exists (projectPath + '\\CMakeLists.txt'):
             projectPath  = self.downloadsPath + '\\' + self.name
             projectBuild = os.getcwd() + '\\Build'
             
-        tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
+        tools.replace_in_file (projectPath + "\\CMakeLists.txt", "PackageTempName", self.name, False)
 
         if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
             packagesPaths = ConanPackages.GetPaths (self, self.PackagesPath, self.Packages)
-            cmake = CMake(self)
+            cmake         = CMake(self)
 
             for packagePathKey, packagePathValue in packagesPaths.items ():
                 cmake.definitions [packagePathKey] = packagePathValue
 
-            cmake.configure(source_dir=projectPath, build_dir=projectBuild)
-            cmake.build()
+            cmake.configure (source_dir = projectPath, build_dir = projectBuild)
+            cmake.build ()
         else:
-            raise Exception('Unsupported platform or compiler')
+            raise Exception ('Unsupported platform or compiler')
         
     def package (self):   
-        projectPath = os.getcwd().replace('\Conan','')
+        projectPath = os.getcwd ().replace ('\Conan','')
         
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+        if not os.path.exists (projectPath + '\\CMakeLists.txt'):
             projectPath = self.downloadsPath + '\\' + self.name
     
-        self.copy('*.h'     , dst='include', src= projectPath + '\\Project' , keep_path=False)
-        self.copy('*.hxx'   , dst='include', src= projectPath + '\\Project' , keep_path=False)
-        self.copy('*.lib'   , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
-        self.copy('*.dll'   , dst='bin'    , src= projectPath + '\Build\bin', keep_path=False)
-        self.copy('*.dylib*', dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
-        self.copy('*.so'    , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
-        self.copy('*.a'     , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
+        self.copy ('*.h'     , dst = 'include', src = projectPath + '\\Project' , keep_path = False)
+        self.copy ('*.hxx'   , dst = 'include', src = projectPath + '\\Project' , keep_path = False)
+        self.copy ('*.lib'   , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.dll'   , dst = 'bin'    , src = projectPath + '\Build\bin', keep_path = False)
+        self.copy ('*.dylib*', dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.so'    , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.a'     , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
 
     def package_info (self):
         self.cpp_info.libs = [self.name]
