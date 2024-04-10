dd265e380a5ff82011bc5fe7d6c390c9a1716eb7
Sylwester <sitkowskis@interia.pl>
2024-04-10 11:29:46 +0200

    Paths

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -22,8 +22,8 @@ class Conan(ConanFile):
     exports         = "*"
     requires        = ["gtest/cci.20210126"]
     downloadPath    = "/home/sylwester/.conan2/download"
-    repoPath        = downloadPath + '/Repo'
-    packagePath     = downloadPath + '/Package'
+    repoPath        = downloadPath + '/Repos'
+    packagePath     = downloadPath + '/Packages'
     packages        = ["logger/1.2", "utils/1.2"]
 
     def layout (self):
