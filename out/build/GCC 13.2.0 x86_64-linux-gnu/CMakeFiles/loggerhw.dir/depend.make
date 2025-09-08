60ce9da1b7d4b18791e7739124dff88b099aec95
ssitkowx <sitkowskis@interia.pl>
2025-09-08 09:57:39 +0200

    Utils 1.3->1.4

--- out/build/GCC 13.2.0 x86_64-linux-gnu/CMakeFiles/loggerhw.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for loggerhw.
-# This may be replaced when dependencies are built.
--- out/build/GCC 13.2.0 x86_64-linux-gnu/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/sylwester/b0f222cb-8386-4622-9d17-261cbd145f18/Home/Projekty/Git/LoggerHw
-# Build directory: /media/sylwester/b0f222cb-8386-4622-9d17-261cbd145f18/Home/Projekty/Git/LoggerHw/out/build/GCC 13.2.0 x86_64-linux-gnu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
